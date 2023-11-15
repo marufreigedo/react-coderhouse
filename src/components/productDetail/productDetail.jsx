a109fcb4880d75e4b1e579870f292bf9a265cb88
¨Marianela <marufreigedo17@gmail.com>
2023-11-15 19:09:46 -0300

    modifico

--- src/components/productDetail/productDetail.jsx
+++ src/components/productDetail/productDetail.jsx
@@ -8,6 +8,7 @@ const ProductDetail = ({ products }) => {
     return (
         <div>
             <h2>{product.name}</h2>
+            <img src={product.image} alt={product.name} /> 
             <p>{product.description}</p>
             <p>{product.price}</p>
             <button onClick={handleGoBack}>Volver a los productos</button>
